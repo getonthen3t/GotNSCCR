f8f0869a1c5315f3f39c2426b91a80e15b84ada8
rasso92 <rasso92@hotmail.com>
2015-08-13 00:40:54 -0600

    Se agrega campo para eliminar clientes temporales. Se corrigen algunos otros problemas.

--- manager/config.php
+++ manager/config.php
@@ -6,9 +6,7 @@
     define( "DB_DSN", "mysql:host=localhost;dbname=gotnsccr" );
     define( "DB_USERNAME", "root" );
     define( "DB_PASSWORD", "" );
-    define('GUSER', 'rasso92@gmail.com'); // GMail username
-    define('GPWD', 'Alejo115150936'); // GMail password
-	define( "CLS_PATH", "class" );
+    define( "CLS_PATH", "class" );
 	
 	//include the classes
         
